2920b657e4b2fe1b9affe86d122c9adf030a09cf
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-06 11:35:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "lib.h"
-
+#include "lib.h.h"
 using namespace std;
+
 int main() {
-  char a;
-  cin >> a;
-  if (convert(a)) {
-    cout << a << endl;
-  } else {
-    cout << "errore" << endl;
-  }
-  return 0;
+    char a=0;
+    cin>>a;
+    if (convert(a)){
+        cout<<a<<endl;
+    }else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
 }
