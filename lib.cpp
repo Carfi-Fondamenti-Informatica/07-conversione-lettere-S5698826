b8e7f789f182614daa09f2de3e3a7cdcfe83b589
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-06 11:09:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,14 +1,1 @@
-//
-// Created by lgian on 06/12/2022.
-//
 
-bool convert (char &a){
-    if (a>=65 && a<=90){
-        a +=32;
-        return true;
-    }else if(a>=97 && a<=122){
-        a -= 32;
-        return true;
-    }
-    return false;
-}
