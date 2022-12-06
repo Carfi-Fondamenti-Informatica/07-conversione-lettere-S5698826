e07cb4261a8eae1a4ca5f065847b5c10b3dd5e27
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-06 11:13:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char z=0;
+  cin>>z;
+  if (convert(z)){
+    cout<<z<<endl;
+  }else{
+    cout<<"errore"<<endl;
+  }
   return 0;
 }
