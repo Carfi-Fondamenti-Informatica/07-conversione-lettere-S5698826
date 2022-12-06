d88afc61b815b9faaf05d877b5d61e98deaa20a2
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-06 11:03:44 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by lgian on 06/12/2022.
 //
 
-#ifndef ESERCITAZIONE_7_LIB_H_H
-#define ESERCITAZIONE_7_LIB_H_H
+#ifndef ESERCITAZIONE_7
+#define ESERCITAZIONE_7
 bool convert (char &);
 #endif //ESERCITAZIONE_7_LIB_H_H
