f4a0e7a2a3e82b0d6258ef39060f13acd7e65f29
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-07 11:32:44 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Gianluca Leone 06/12/2022.
 //
 
-#ifndef ESERCITAZIONE_7_LIB_H
-#define ESERCITAZIONE_7_LIB_H
+#ifndef ESERCITAZIONE_7_LIB_H_H
+#define ESERCITAZIONE_7_LIB_H_H
 bool convert (char &);
-#endif //ESERCITAZIONE_7_LIB_H
+#endif //ESERCITAZIONE_7_LIB_H_H
