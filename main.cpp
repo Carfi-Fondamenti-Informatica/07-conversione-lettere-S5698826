10ea428788426c007950c492205af00b01c76f7e
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-07 11:48:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include "lib.h"
-using namespace std;
 
-int main() {
-    char a;
-    cin>>a;
-    if (convert( a)){
-        cout<<a<<endl;
-    }else{
-        cout<<"errore"endl;
-    }
-    
-    
