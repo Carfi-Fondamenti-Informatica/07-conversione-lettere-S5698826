0aeef5381ea3e7e79490087cbff3aeadd08f84f9
S5698826 <117889839+S5698826@users.noreply.github.com>
2022-12-07 11:48:15 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,1 @@
-//
-// Created by Gianluca Leone on 06/12/2022.
-//
 
-#include "lib.h"
-bool convert (char &a){
-    if (a>=65 && a<=90){
-        a +=32;
-        return true;
-    }else if (a>=97 && a<=122){
-        a-=32;
-        return true;
-    }
-    return false;
-}
